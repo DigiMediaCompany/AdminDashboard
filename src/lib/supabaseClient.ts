d0db41f26e8951f3421f73020f7694e499174c0a
Mjho <hoang.nguyen@molinetwork.com>
2025-07-01 19:02:05 +0700

    trying to add Supabase

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+// import type { Database } from '../types/supabase';
+
+const supabaseUrl = import.meta.env.SUPABASE_ADMIN_DASHBOARD_API_URL as string;
+const supabaseAnonKey = import.meta.env.SUPABASE_ADMIN_DASHBOARD_ANON_KEY as string;
+
+// export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import "flatpickr/dist/flatpickr.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { SupabaseProvider } from "./context/SupabaseContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <AppWrapper>
-        <App />
-      </AppWrapper>
-    </ThemeProvider>
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider>
+            <SupabaseProvider>
+                <AppWrapper>
+                    <App />
+                </AppWrapper>
+            </SupabaseProvider>
+        </ThemeProvider>
+    </StrictMode>,
 );
