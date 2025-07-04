83f4c0f215f0903d4d4b3be211850640f884f3a6
Mjho <hoang.nguyen@molinetwork.com>
2025-07-04 12:49:25 +0700

    change env var name to fit CF + Vite

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 // import type { Database } from '../types/supabase';
 
 const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_ADMIN_DASHBOARD_API_URL as string;
+// TODO: need to change var -> secret
 const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ADMIN_DASHBOARD_ANON_KEY as string;
 
 // export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
