ae147fa7da1f1f2f248feb1fbe78acb69370869a
Mjho <hoang.nguyen@molinetwork.com>
2025-09-02 04:18:33 +0700

    relocation

--- src/utils/helper.ts
+++
@@ -1,8 +0,0 @@
-export function snakeToTitleCase(input: string): string {
-    return input
-        .split('_')
-        .map(word =>
-            word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-        )
-        .join(' ');
-}
